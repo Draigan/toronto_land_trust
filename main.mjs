9fe5a44fa4bde3d5a0ec016de9d072ae02e56b73
Draigan <draigan.lefebvre@gmail.com>
2022-09-18 08:48:40 -0400

    Ok ready to deploy! Spent a whole day beating my head against the keyboard over some really annoying bug about copying todays list to yesterdays list. Absolutely infuriating. Fixed it by copying the files after sorting and downloading.

--- main.mjs
+++ main.mjs
@@ -1,32 +1,93 @@
- import * as download from "./download.mjs"; 
+import energize from "./download.mjs";
 import * as mailer from "./mailer.mjs";
 import * as sorter from "./sorter.mjs";
 import moment from "moment";
 import fs from "fs";
 import pastItems from "./pastItems.json" assert {type:"json"};
 
+
+
 let date = moment().format("D") + moment().format("MMM") + moment().format("YYYY");
 
 
-async function
+energize("todays-list").then(() => { sorter.sort().then( listArray => {
+    console.log('finished sorting')
+    const applicationList = listArray[0];
+    const statusList = listArray[1];
+
+ //Updating the status of items from pastList with their current status. 
+    statusList.forEach( statusItem => {
+        pastItems.find( pastItem => statusItem.APPLICATION_NUMBER == pastItem.APPLICATION_NUMBER ).STATUS = statusItem.STATUS;
+    })
+
+    //Adding items from application list to the pastItems List
+applicationList.forEach(item => {
+pastItems.push(item);
+});
+
+    let data = JSON.stringify(pastItems);
+fs.writeFileSync('pastItems.json', data);
+
+
+ console.log("APPLICATIONS LIST _____________");
+ console.log(applicationList);
+
+ console.log("STATUS LIST _____________");
+ console.log(statusList);
+
+ if (statusList.length != 0 || applicationList.length != 0 ){
+ console.log("Emailing...");
+ fs.copyFileSync("./yesterdays-list.json", `./history/${date}.json`);
+ fs.copyFileSync("./todays-list.json", "./yesterdays-list.json");
+ mailer.mail(applicationList,statusList).catch(err => {
+
+    mailer.mailError(err);
+    console.log(err);
+    
+    
+    }).catch(err => {
+
+        mailer.mailError(err);
+        console.log(err);
+        
+        
+        });} else { console.log('There are no new entries to email')}
+ } );})
+
+
+/* fs.unlink('./yesterdays-list.json', (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("File is deleted.");
+});
+
+fs.copyFileSync("./todays-list.json", `./yesterdays-list.json`) 
+ */
+
+
+
+/* 
+
+export function
 mainFunction()
 {
-    fs.copyFileSync("./yesterdays-list.json", `./history/${date}.json`)
-    fs.copyFileSync("./todays-list.json", `./yesterdays-list.json`)
-   
+ 
 
-    console.log( "Downloading todays list...");
-    const downloaded= await download.energize('todays-list').catch(err => {
+
+    const downloaded = await download.energize('todays-list').catch(err => {
 
         mailer.mailError(err);
         console.log(err);
         
         
         });
-    console.log("Sorting list and requesting lon and lat from api...")
+ */
 
-
-    const sort = await sorter.sort().then( listArray => {
+/* 
+    const sort = sorter.sort().then( listArray => {
+        console.log('finished sorting')
         const applicationList = listArray[0];
         const statusList = listArray[1];
 
@@ -49,7 +110,7 @@ fs.writeFileSync('pastItems.json', data);
      console.log("STATUS LIST _____________");
      console.log(statusList);
 
-     if (statusList.length != 0 ){
+     if (statusList.length != 0 || applicationList.length != 0 ){
      console.log("Emailing...");
      mailer.mail(applicationList,statusList).catch(err => {
 
@@ -70,7 +131,7 @@ fs.writeFileSync('pastItems.json', data);
 
 };
 
-mainFunction();
+ */
 
 
 
--- sorter.mjs
+++ sorter.mjs
@@ -8,168 +8,149 @@ new entries.
 5. Checks that the coordinates of a list item falls within the geojson shape of torontos ward 4 and eliminates it from the list if not.
  */
 import fetch from 'node-fetch';
-import * as d3  from "d3-geo";
+import * as d3 from "d3-geo";
 import geoWard from "./geoward4.json" assert { type: "json" };
 import yesterdaysList from "./yesterdays-list.json" assert {type: "json"};
-import todaysList from "./todays-list.json" assert {type: 'json'}; 
+import todaysList from "./todays-list.json" assert {type: 'json'};
 import https from "https";
 import { resolve } from 'path';
-import pastItems from "./pastItems.json" assert {type:"json"};
+import pastItems from "./pastItems.json" assert {type: "json"};
 
-export function 
-sort(){
+export function
+    sort() {
 
-    return new Promise((resolve,reject) =>
-    {
+    return new Promise((res, rej) => {
+        if (false == true) { rej}
 
-        if (true == false) {reject} //temp
-
-        const postalCode = [ 'M6K', 'M6H', 'M6P', 'M6R', 'M6J', 'M5X', 'M5J', 'M5K', 'M6L', 'M3C', 'M5H'];
+        const postalCode = ['M6K', 'M6H', 'M6P', 'M6R', 'M6J', 'M5X', 'M5J', 'M5K', 'M6L', 'M3C', 'M5H'];
 
         const oldList = cleanUpList(yesterdaysList);
         const newApplicationList = cleanUpList(todaysList);
         const newStatusList = cleanUpList(todaysList);
         const refinedNewStatusList = [];
-               
+
         function
-        cleanUpList(list)
-        {
-            return list.filter( item => 
-            {
-                for ( let code of postalCode)
-                {
-                    if ( code == item.POSTAL ) 
-                    {
+            cleanUpList(list) {
+            return list.filter(item => {
+                for (let code of postalCode) {
+                    if (code == item.POSTAL) {
                         return true;
                     }
-                } 
-                
-                    return false; 
-                
-            } ).map( (item) =>
-                 {
-                    return {
-                        'APPLICATION_NUMBER' : item.APPLICATION_NUMBER,
-                        'DESCRIPTION' : item.DESCRIPTION,
-                        'ID': item._id,
-                        'POSTAL' : item.POSTAL,
-                        "STATUS" : item.STATUS,
-                        'APPLICATION_TYPE' : item.APPLICATION_TYPE,
-                        'DATE_SUBMITTED' : item.DATE_SUBMITTED,
-                        
-                        'REFERENCE_FILE_NUMBER': item.REFERENCE_FILE_NUMBER,
-                        'HEARING_DATE': item.HEARING_DATE,
-                  
-                        "STREET_NAME": item.STREET_NAME, 
-                        "STREET_NUM": item.STREET_NUM, 
-                        "STREET_TYPE": item.STREET_TYPE,
-                        "LON" : null,
-                        "LAT" : null,
-                        "COORDINATES" : []
-                    }
-                 }
-                 )
                 }
-         
 
-                
+                return false;
+
+            }).map((item) => {
+                return {
+                    'APPLICATION_NUMBER': item.APPLICATION_NUMBER,
+                    'DESCRIPTION': item.DESCRIPTION,
+                    'ID': item._id,
+                    'POSTAL': item.POSTAL,
+                    "STATUS": item.STATUS,
+                    'APPLICATION_TYPE': item.APPLICATION_TYPE,
+                    'DATE_SUBMITTED': item.DATE_SUBMITTED,
+
+                    'REFERENCE_FILE_NUMBER': item.REFERENCE_FILE_NUMBER,
+                    'HEARING_DATE': item.HEARING_DATE,
+
+                    "STREET_NAME": item.STREET_NAME,
+                    "STREET_NUM": item.STREET_NUM,
+                    "STREET_TYPE": item.STREET_TYPE,
+                    "LON": null,
+                    "LAT": null,
+                    "COORDINATES": []
+                }
+            }
+            )
+        }
+
 
         function
-        findChangedStatus()
-        {
-            for (let i = 0; i < pastItems.length; i ++)
-            {
-            for (let j = newStatusList.length - 1;  j > 0; j--)   //working backwards to stop at the most recent application number
-            {
-                if (pastItems[i].APPLICATION_NUMBER == newStatusList[j].APPLICATION_NUMBER)
+            findChangedStatus() {
+            for (let i = 0; i < pastItems.length; i++) {
+                for (let j = newStatusList.length - 1; j > 0; j--)   //working backwards to stop at the most recent application number
                 {
-                    if (pastItems[i].STATUS != newStatusList[j].STATUS)
-                    {
-                        refinedNewStatusList.push(newStatusList[j]);
-                        break;
+                    if (pastItems[i].APPLICATION_NUMBER == newStatusList[j].APPLICATION_NUMBER) {
+                        if (pastItems[i].STATUS != newStatusList[j].STATUS) {
+                            refinedNewStatusList.push(newStatusList[j]);
+                            break;
+                        }
                     }
                 }
             }
         }
-        } 
 
         findChangedStatus();
 
-                
 
-        
-     function
-        getRidOfCopies(originalList,modifiedList)
-        {
-        for (let i = 0; i < modifiedList.length; i++)
-        {
-            for (let item of originalList)
-            {
-                if (modifiedList[i].APPLICATION_NUMBER == item.APPLICATION_NUMBER ){
-                    modifiedList.splice(i,1);
-            } 
-        
+
+
+        function
+            getRidOfCopies(originalList, modifiedList) {
+            for (let i = 0; i < modifiedList.length; i++) {
+                for (let item of originalList) {
+                    if (modifiedList[i].APPLICATION_NUMBER == item.APPLICATION_NUMBER) {
+                        modifiedList.splice(i, 1);
+                    }
+
+                }
             }
         }
-        }
-        getRidOfCopies(oldList,newApplicationList); 
-        
-        
-        
+        getRidOfCopies(oldList, newApplicationList);
+
+
+
         const promiseArray = [];
-        
-            const httpRequest = (url,index) => 
-            {
-                return new Promise((resolve,reject) =>
-                {
-        
-                    fetch(url).then( response => response.json())
-                    .then( json => {
-                
+
+        const httpRequest = (url, index) => {
+            return new Promise((resolve, reject) => {
+
+                fetch(url).then(response => response.json())
+                    .then(json => {
+
                         //Adding lon and lat to my list item
-               
+
                         newApplicationList[index].LON = json[0].lon;
                         newApplicationList[index].LAT = json[0].lat;
                         newApplicationList[index].COORDINATES.push(json[0].lon, json[0].lat);
-                  
+
                         resolve();
-                }).catch(reject);
+                    }).catch(reject);
             });
         };
-        
-        for (let i = 0; i< newApplicationList.length; i++)
-        {
-        let url = `https://nominatim.openstreetmap.org/search?q=${newApplicationList[i].STREET_NUM}+${newApplicationList[i].STREET_NAME}+${newApplicationList[i].STREET_TYPE},+toronto&format=json`;
-        
-           setTimeout( () => {promiseArray.push(httpRequest(url,i))}, 3000 * i);
-        
+
+        for (let i = 0; i < newApplicationList.length; i++) {
+            let url = `https://nominatim.openstreetmap.org/search?q=${newApplicationList[i].STREET_NUM}+${newApplicationList[i].STREET_NAME}+${newApplicationList[i].STREET_TYPE},+toronto&format=json`;
+
+            setTimeout(() => { promiseArray.push(httpRequest(url, i)) }, 3000 * i);
+
         }
-        setTimeout( () => {Promise.all(promiseArray).then(()=> 
-            {
-              for (let i = 0; i < newApplicationList.length; i++)
-                {
+        setTimeout(() => {
+            Promise.all(promiseArray).then(() => {
+                for (let i = 0; i < newApplicationList.length; i++) {
                     //checks to see if listings long and lat falls within ward4's geojson
-                if (d3.geoContains(geoWard,newApplicationList[i].COORDINATES) == false) 
-                {
-                     newApplicationList.splice(i,1)
-                }; 
-            } 
-              
-     
+                    if (d3.geoContains(geoWard, newApplicationList[i].COORDINATES) == false) {
+                        newApplicationList.splice(i, 1)
+                    };
+                }
+
+
                 let listArray = [];
                 listArray.push(newApplicationList, refinedNewStatusList);
-                console.log(newApplicationList)
-                console.log(refinedNewStatusList)
-                 resolve(listArray);
-        
-            })}, newApplicationList.length * 3001)
+
+/*       console.log(newApplicationList)
+          console.log(refinedNewStatusList)  */
+                res(listArray);
+
+            })
+        }, newApplicationList.length * 3001)
     });
 
 }
 
 export default sort;
-sort();
 
+sort();
 /*                                                                                      Failed attempt do to http module not parsing my json properly? something about jsonP? Very annoying
  that fetch worked but https  module did not.                                 
 const promiseArray = [];
